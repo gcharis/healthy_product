11931af08246217862cdcecf139b145279bc1361
gcharis <xaris.giar@hotmail.com>
2017-12-01 00:14:12 +0200

    hue

--- public/javascripts/services/productsServices.js
+++ public/javascripts/services/productsServices.js
@@ -2,7 +2,7 @@ app.service('$products', function ($http) {
     return {
         getAll() {
             return new Promise((resolve, reject) => {
-                $http.get(URL + '/products/all')
+                $http.get(`${URL}/products/all`)
                     .then(res => {
                         if (res.status === 500) return reject(res.data.err)
                         resolve(res.data)
