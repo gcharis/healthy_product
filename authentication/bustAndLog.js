42a4a88708ef5a888b2160d1b353a0919e0c3672
gcharis <xaris.giar@hotmail.com>
2017-12-16 19:34:14 +0200

    test

--- authentication/bustAndLog.js
+++ authentication/bustAndLog.js
@@ -7,16 +7,19 @@ module.exports = function(req, res, next) {
 	if (!token) return res.send({ message: 'Χρείαζεστε έγκριση για αυτή την ενέργεια.' });
 
 	// Verify admin
-	authentication.verifyAdmin(token).then((results) => next())// An err object is created when the authentication fails
-	.catch((err) => {
-		let intruderIP = req.headers['x-appengine-user-ip'] || req.connection.remoteAddress;
+	authentication
+		.verifyAdmin(token)
+		.then((results) => next()) // An err object is created when the authentication fails
+		.catch((err) => {
+			let intruderIP = req.headers['x-appengine-user-ip'] || req.connection.remoteAddress;
 
-		console.error(`Authentication failed for IP: ${intruderIP} on ${new Date()}`);
+			console.error(`Authentication failed for IP: ${intruderIP} on ${new Date()}`);
 
-		const newLog = { date: new Date(), content: `Αποτυχημένη προσπάθεια σύνδεσης για την IP: ${intruderIP}` };
+			const newLog = { date: new Date(), content: `Αποτυχημένη προσπάθεια σύνδεσης για την IP: ${intruderIP}` };
 
-		logger.updateLogs(newLog).catch((err) => res.status(500));
+			logger.updateLogs(newLog).catch((err) => res.status(500));
 
-		res.status(500).send(err.message);
-	});
+			res.status(500).send(err.message);
+			console.log('remove this');
+		});
 };
