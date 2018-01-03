d8f7307a76b68485c21ae4a5a42d7e6619a6603b
icantwritecode <andrchas@gmail.com>
2018-01-03 16:09:22 +0200

    Edit Product view && Register View

--- public/javascripts/controllers/productInfoCtrl.js
+++ public/javascripts/controllers/productInfoCtrl.js
@@ -4,7 +4,10 @@ app.controller('productInfo', function($scope, $routeParams, $products) {
 	function getProductInfo() {
 		$products
 			.getBySlug($routeParams.slug)
-			.then((product) => ($scope.product = product))
+			.then((product) => {
+				$scope.product = product;
+				console.log(product);
+			})
 			.catch((res) => ($scope.errMsg = res.data));
 	}
 });
