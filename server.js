1550d99713103911630c3f9462fe68ccf1929350
gcharis <xaris.giar@hotmail.com>
2018-01-23 00:32:27 +0200

    getting site done

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ const config = require('./config.js');
 const app = express();
 const port = process.env.PORT || config.port;
 
-app.use(function (req, res, next) {
-	res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+app.use(function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', 'http://localhost:9000');
 	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 	next();
@@ -25,13 +25,17 @@ app.use(function (req, res, next) {
 app.use(morgan('dev'));
 
 // Body Parser middleware
-app.use(bodyParser.urlencoded({
-	limit: '50mb',
-	extended: true
-}));
-app.use(bodyParser.json({
-	limit: '50mb'
-}));
+app.use(
+	bodyParser.urlencoded({
+		limit: '50mb',
+		extended: true
+	})
+);
+app.use(
+	bodyParser.json({
+		limit: '50mb'
+	})
+);
 
 // Ejs middleware
 app.set('view engine', 'ejs');
