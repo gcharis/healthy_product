4d5de1f071ec26e8adf23df53df51217f9d02a2b
icantwritecode <andrchas@gmail.com>
2018-01-25 23:09:27 +0200

    Shipping view & modal

--- public/javascripts/controllers/shippingsCtrl.js
+++ public/javascripts/controllers/shippingsCtrl.js
@@ -1,3 +1,36 @@
-app.controller('shippings', function($scope, $shippings) {
-	$shippings.getAll().then((shippings) => console.log(shippings)).catch((err) => console.warn(err));
+app.controller('shippings', function ($scope, $shippings, $uiHandler, $timeout) {
+	showShipping()
+
+	$scope.registerShipping = (newShipping) => {
+		$shippings
+			.register(newShipping)
+			.then((product) => {
+				clearShippingRegistrationForm();
+				showShippings();
+			})
+			.catch((res) => ($scope.message = res.data));
+	};
+	$scope.addNewShipping = () => {
+		$scope.addingNewShipping = true;
+		$timeout(() => $uiHandler.openModalById('newShippingModal'), 0);
+	};
+
+
+	function initiateNewShipping() {
+		$scope.newShipping = {
+
+		};
+	}
+
+	function clearShippingRegistrationForm() {
+		$scope.newShipping = {};
+		$uiHandler.hideModalById('registerModal');
+	}
+
+	function showShipping() {
+		$shippings.getAll().then((shippings) => console.log(shippings)).catch((err) => console.warn(err));
+	}
+
+	$scope.openModalById = (id) => $uiHandler.openModalById(id);
+	$scope.hideModalById = (id) => $uiHandler.hideModalById(id);
 });
