4e053269d60546172e1303a4958e26a7e620896c
gcharis <xaris.giar@hotmail.com>
2018-01-31 21:52:47 +0200

    merging

--- database/models/Product.js
+++ database/models/Product.js
@@ -5,24 +5,25 @@ const autoIncrement = require('mongoose-auto-increment');
 autoIncrement.initialize(mongoose.connection);
 
 let productSchema = new mongoose.Schema({
-	name: { type: String, unique: true, required: true },
-	slug: { type: String, unique: true, required: true },
-	sku: { type: String, unique: true, required: true },
+	name: { type: String, unique: true, required: [ true, `Το πεδίο 'Όνομα' είναι υποχρεωτικό` ] },
+	slug: { type: String, unique: true },
+	sku: { type: String, unique: true, required: [ true, `Το πεδίο 'Κωδικός' είναι υποχρεωτικό` ] },
 	category: { type: Array, default: [] },
 	description: String,
-	price: { type: Number, required: true },
+	price: { type: Number, required: [ true, `Το πεδίο 'Τιμή' είναι υποχρεωτικό` ] },
 	salesPrice: Number,
-	weight: { type: Number, required: true, default: 0 },
+	weight: { type: Number, required: [ true, `Το πεδίο 'Βάρος' είναι υποχρεωτικό` ] },
 	amountForSale: String,
 	stock: Number,
 	creationDate: { type: Date, default: Date.now },
 	expirationDate: Date,
 	images: [ String ],
-	featuredImage: String
+	featuredImage: String,
+	priority: Number
 });
 
 productSchema.pre('validate', function(next) {
-	if (!this.name) throw new ReferenceError(`Field 'name' for new product is not filled`);
+	if (!this.name) throw new ReferenceError(`Το πεδίο 'Όνομα' είναι υποχρεωτικό`);
 	this.slug = this.name.toGreeklish();
 	next();
 });
