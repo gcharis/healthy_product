7fd998ff96674a4e9c66ba07ddf9cbfa294695c3
icantwritecode <andrchas@gmail.com>
2018-02-14 21:02:45 +0200

    env proceses

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	apps: [
+		{
+			env: {
+				SECRET_KEY: 'super-secret',
+				PORT: 4000,
+				REGISTER_KEY: 'super-secret-register',
+				CAPTCHA_KEY: '6LceaEYUAAAAAEYCEAq6H9kWPh6dN0Ui5kjsBh-s'
+			}
+		}
+	]
+};
