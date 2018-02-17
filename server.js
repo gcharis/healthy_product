9069b2214c6d41497ac51aa0314469efc520945f
elissaioskon <elissaioskon@gmail.com>
2018-02-17 18:44:30 +0000

    nothing

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ require('./schedule_jobs/productViewCounterReset.js');
 const app = express();
 
 app.use(function(req, res, next) {
-	res.header('Access-Control-Allow-Origin', 'http://localhost:9000');
+	res.header('Access-Control-Allow-Origin', '*');
 	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 	next();
