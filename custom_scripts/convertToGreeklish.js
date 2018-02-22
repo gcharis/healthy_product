2cf8d02f84da43d454ea4cd7e07556d622fd5fdf
elissaios <elissaios13@hotmail.com>
2018-02-22 21:01:06 +0200

    change module toGreeklish to not return - at numbers slug

--- custom_scripts/convertToGreeklish.js
+++ custom_scripts/convertToGreeklish.js
@@ -71,6 +71,17 @@ module.exports = function toGreeklish() {
 					return 'w';
 				case 'ώ':
 					return 'w';
+				case '0':
+				case '1':
+				case '2':
+				case '3':
+				case '4':
+				case '5':
+				case '6':
+				case '7':
+				case '8':
+				case '9':
+					return letter;
 				default:
 					return '-';
 			}
